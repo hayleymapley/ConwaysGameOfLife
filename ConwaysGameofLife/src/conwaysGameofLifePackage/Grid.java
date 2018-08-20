9baa70a1998993e2922cbff3d9e2db82e541578c
cholmojuli <cholmojuli@gs1808.AD.ECS.VUW.AC.NZ>
2018-08-21 09:58:09 +1200

    Added isEmpty method

--- ConwaysGameofLife/src/conwaysGameofLifePackage/Grid.java
+++ ConwaysGameofLife/src/conwaysGameofLifePackage/Grid.java
@@ -3,28 +3,41 @@ package conwaysGameofLifePackage;
 import java.util.HashMap;
 import java.util.Map;
 
+import javafx.scene.Group;
+import javafx.scene.Node;
+
 public class Grid {
 	private Map<Position, AliveCell> aliveCells = new HashMap<>();
+	
+	//private Group cellGroup = new Group();
 
 	public Grid() {
 		
 	}
 	
-	/**
-	 * updates each cell withing the Grids Collection
-	 * 
-	 */
-	public void updateAliveCells() {
-		//get live cells and update each cell object. 
-		//test
-	}
+//	public boolean isEmpty(Thing t) {
+//		for (Node n : thingGroup.getChildren()) {
+//			if (!n.equals(t)) {
+//				if (t.getBoundsInParent().intersects(n.getBoundsInParent())) {
+//					return false;
+//				}
+//			}
+//
+//		}
+//		System.out.println("Obstacle are NOT colliding");
+//		return true;
+//	}
 
 	
-//	public void drawCells() {		//this has been moved to UI??
-//		// draws cells into grid.
-//		//includes color black/white
-//	}
-	
+	public void updateAliveCells() {
+		//get live cells and update each cell object. 
+		
+	}
+
+	public void drawCells() {
+		// draws cells into grid.
+		//includes color black/white
+	}
 	
 	public void addCell(Position position, AliveCell cell) {
 		aliveCells.put(position, cell);
