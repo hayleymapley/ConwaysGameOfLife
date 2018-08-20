ab9fd1515bb929bbd78e08c6c33ea9390e78b651
OPelham <ojptwenty@gmail.com>
2018-08-20 14:46:50 +1200

    Start off UI class

--- ConwaysGameofLife/src/conwaysGameofLifePackage/UI.java
+++ ConwaysGameofLife/src/conwaysGameofLifePackage/UI.java
@@ -1,7 +1,31 @@
 package conwaysGameofLifePackage;
 
-public class UI {
+import javafx.application.Application;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.stage.*;
 
+public class UI extends Application{
+	private int startWidth = 600;
+	private int startHeight = 400;
+	
+	private ScrollPane pane = new ScrollPane();
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.setTitle("Game of Life");
+		primaryStage.show();
+		
+	}
+	
+	public void updateGrid() {
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
