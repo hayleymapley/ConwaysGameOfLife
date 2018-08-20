2c554ae7f0df8849c7bb2b100bb75f24fd321303
OPelham <ojptwenty@gmail.com>
2018-08-20 17:49:45 +1200

    More javadoc updates

--- ConwaysGameofLife/src/conwaysGameofLifePackage/UI.java
+++ ConwaysGameofLife/src/conwaysGameofLifePackage/UI.java
@@ -101,24 +101,44 @@ public class UI extends Application{
 		});
 	}
 	
+	/**
+	 * Sets attributes for buttons
+	 */
 	public void initialiseButtons() {
 		playPause.setText("Play/Pause");
 		restart.setText("Restart");
 		quit.setText("Quit");
 	}
+	/**
+	 * Method to set up initial state of the simulation
+	 * <p>
+	 * creates grid, creates initial AliveCells, Initial Draw() of simulation
+	 */
 	public void initialiseGrid() {
 		Grid worldGrid = new Grid();
-		AliveCell cell = new AliveCell();
+		//put following two lines into own method
+		AliveCell cell = new AliveCell(); 				// TODO: make better way of initialising positions of cells at begenning of simulation (mouse click or random 'seed')
 		worldGrid.addCell(generatePosition(), cell); 	// adds the cell to the grid
 		drawCells(worldGrid.getAliveCells()); 			// calls on HashMap in grid
 	}
 	
+	/**
+	 * Method to update all AliveCells in the Grids Collection
+	 * <p>
+	 * first will remove all dead cells
+	 * second will call update on all remaining cells
+	 */
 	public void updateGrid() {
 		//TODO: everything
 		//remove dead cells
 		//call update on alive cells
 	}
 	
+	/**
+	 * iterates through collection of all alive cells and draws them at their location (by adding them to simulation Pane)
+	 * 
+	 * @param aliveCells - collection of all alive cells from the Grids 
+	 */
 	public void drawCells(Map<Position, AliveCell> aliveCells) {
 		//TODO: everything
 		for (Map.Entry<Position, AliveCell> e : aliveCells.entrySet()) {
@@ -128,12 +148,20 @@ public class UI extends Application{
 		}
 	}
 	
+	/**
+	 * Generates a random Position object for an Alive cell, used for initialising simulation
+	 * 
+	 * @return - returns a new Position Object
+	 */
 	public Position generatePosition() {
 		//TODO make realtive to col/row eg height * row
 		Position newPosition = new Position(20, 20);
 		return newPosition;
 	}
 	
+	/**
+	 * 
+	 */
 	public void initialisePanes() {
 		initialiseButtons();
 		controlBox.getChildren().addAll(playPause, restart, quit);
